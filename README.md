4ec8f1481cb2a2eaa7cf0a6c8eed791b7f4b1644
Alicja Raszkowska <malavarena@gmail.com>
2018-03-03 01:10:02 -0800

    Set up the project

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # HTML + CSS resume
 Pure HTML and CSS resume.
+
+# How to install
+
+```
+npm install
+```
+
+# How to use
+
+```
+gulp
+```
+
+Refresh on changes.
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var connect = require('gulp-connect');
+
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var scss = require('postcss-scss');
+var postcssProcessors = [
+  autoprefixer( { browsers: ['last 2 versions', 'ie > 10'] } )
+]
+
+var nunjucksRender = require('gulp-nunjucks-render');
+
+// generate main.css
+gulp.task('addCSS', function(callback) {
+    return gulp.src('src/sass/main.scss')
+        .pipe(
+           postcss(postcssProcessors, {syntax: scss})
+        )
+        .pipe(
+            sass({ outputStyle: 'compressed' })
+            .on('error', gutil.log)
+        )
+        .pipe(gulp.dest('build/css/'));
+});
+
+// generate index.html
+gulp.task('nunjucks', ['addCSS'], function() {
+    return gulp.src('src/*.nunjucks')
+        .pipe(
+            nunjucksRender({
+                path: ['src/', 'build/css/']
+            })
+            .on('error', gutil.log)
+        )
+        .pipe(gulp.dest('build/'));
+});
+
+// set up server
+gulp.task('connect', function() {
+    connect.server({
+        port: 8000,
+        root: 'build',
+        livereload:true
+    });
+});
+
+// watch files
+var filesToWatch = [
+    'src/sass/**/*.scss',
+    'src/partials/*.nunjucks',
+    'src/*.nunjucks',
+]
+
+gulp.task('watch', function() {
+    gulp.watch(filesToWatch, ['nunjucks']);
+});
+
+// run
+gulp.task('default', ['connect', 'nunjucks', 'watch']);
